0486fab3979e412bd5bb88fa44e156ecf21d170d
RuslanAbdullaev <ruslan.abdullaev060994@gmail.com>
2024-06-06 23:37:34 +0300

    code optimization

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -7,12 +7,12 @@ from bs4 import BeautifulSoup
 
 urls = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2024/"
 columns_name = "HourlyDryBulbTemperature"
+date_search = "2024-06-05 20:15"
 
 
 def url_check(url) -> str | None:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    date_search = "2024-06-05 20:15"
     for row in soup.find_all("tr"):
         if date_search in row.text:
             return url + row.find("a").text
@@ -27,20 +27,23 @@ def download__read_file(url) -> pd.DataFrame:
                 with open(filename, "wb") as file:
                     file.write(response.content)
                 return pd.read_csv(filename)
-            else:
-                print(f"Download failed: {filename}")
+
     except Exception as e:
         print(e)
+        raise Exception(f"Download failed: {e}")
 
 
 def main():
     with ThreadPoolExecutor(max_workers=4) as executor:
         urls_list = [urls]
         future = list(executor.map(url_check, urls_list))
-        result = list(executor.map(download__read_file, future))
+        try:
+            result = list(executor.map(download__read_file, future))
 
-        result_df = pd.concat(result, ignore_index=True)
-        print(result_df[columns_name].max())
+            result_df = pd.concat(result, ignore_index=True)
+            print(result_df[columns_name].max())
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
